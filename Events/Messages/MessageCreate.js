07a12d7f05a1e0bd14010f8d97eb6095f4c6ff15
bestlegendary124816 <105922083+bestlegendary124816@users.noreply.github.com>
2023-03-22 11:46:58 +0400

    Update MessageCreate.js

--- Events/Messages/MessageCreate.js
+++ Events/Messages/MessageCreate.js
@@ -13,6 +13,20 @@ module.exports = {
  * @param {Client} client 
  */
   async execute(message, client) {
+const Prefix = "?";
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    if (message.content.toLowerCase().startsWith(Prefix)) {
+      const args = message.content.slice(Prefix.length).trim().split(/ +/g);
+      const commandName = args.shift();
+      const command = client.commands.get(commandName);
+      if (!command) return;
+      try {
+        command.run(client, message, args);
+      } catch {
+        return;
+      }
+    }
     if (message.author.bot || !message.guild) return;
     //chatbot
     Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
